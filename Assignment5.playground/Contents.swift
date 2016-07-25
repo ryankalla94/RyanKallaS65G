8ee68dcdca1b080a3f2b2793039fd7190dd8fd54
ryankalla94 <ryankalla94@gmail.com>
2016-07-25 15:42:37 -0400

    Assignment5 with Project Bonus Proposal

--- Assignment5.playground/Contents.swift
+++ Assignment5.playground/Contents.swift
@@ -34,3 +34,8 @@ isLeap(1900)
 isLeap(2000)
 
 
+/* Project Bonus Proposal
+   (dont know if this is where I should put this, I'll also email Van)
+I want to do something similar to mutation. I was thinking of making a rule change so that something happens when the game reaches a stable state to keep the game going. I was thinking either randomizing the board, or bringing to life all the neighbors of living cells when the board stabilizes. I like the idea of the latter, but I would have to experiment to make sure all the cells dont just die out from overpopulation.
+ There seems to be two kinds of stabilization, one where grid = step(grid) (this includes when the grid is completely dead), and one where grid = step(step(grid)) (where the grid alternates between two states). I would have to handle both these cases, and change the board so the game keeps going. With this rule change, the game would (hopefully) be endless.
+ */
